c5646689883e5e8a18f20723583a1efd0c075710
lazarodamasceno <lazarolawrence2@gmail.com>
2025-08-13 02:59:41 -0300

    Substituiu algumas imagens

--- analise-dados/diagrama_barras_orçamento_justiça_2010_2025.R
+++ analise-dados/diagrama_barras_orçamento_justiça_2010_2025.R
@@ -27,8 +27,9 @@ read_xlsx('orçamento_justiça_2010_2025.xlsx') %>%
     ggplot(aes(x = Anos, y = Valores, fill = Orgao)) +
     geom_col()  +
     scale_fill_brewer(palette = 'Dark2') +
-    scale_x_discrete(
-        labels = seq(2010, 2025)
-    ) +
-    labs(y = 'Porcentagem') +
-    theme(axis.text.x = element_text(angle = 270, hjust = 1))
+    scale_x_discrete(labels = seq(2010, 2025)) +
+    labs(y = 'Porcentagem', fill = 'Órgãos') +
+    theme(
+        axis.text.x = element_text(angle = 270, hjust = 1),
+        legend.title = element_text()
+    )
---
+++ analise-dados/diagrama_linhas_orçamento_justiça_2010_2025.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(tidyr)
+library(readxl)
+
+read_xlsx('orçamento_justiça_2010_2025.xlsx')
